9a4b446dc847120ae702319939969fd25b7815aa
Alejandro Gómez <alejandro@dialelo.com>
2016-12-09 10:26:48 +0100

    Include favicon in prerendered page

--- src/org/build.clj
+++ src/org/build.clj
@@ -22,6 +22,11 @@
     [:meta
      {:name "viewport", :content "width=device-width, initial-scale=1"}]
     [:link
+     {:rel "icon",
+      :type "image/png"
+      :href
+      "img/favicon.png"}]
+    [:link
      {:rel "stylesheet",
       :href
       "https://cdnjs.cloudflare.com/ajax/libs/octicons/3.5.0/octicons.min.css"}]
