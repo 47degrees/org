2e8e4b129bdb66dbf6405137df70353053d00128
Alejandro Gómez <alejandro@dialelo.com>
2017-01-04 15:03:45 +0100

    Checkout master before commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ git config --global user.name "Travis CI"
 git config --global user.email "al.gg@47deg.com"
 
 # Commit built site
+git checkout master
 git add docs
 git commit -m "[skip ci] Generate site"
 git push https://47deg:$GITHUB_API_KEY@github.com/47deg/org master
