a0fb2815f10938de492f69a55e04bfe76a45924a
Alejandro Gómez <alejandro@dialelo.com>
2017-01-04 14:58:14 +0100

    Configure git before using it

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# Configure git
+git config --global user.name "Travis CI"
+git config --global user.email "al.gg@47deg.com"
+
 # Commit built site
 git add docs
-git commit --author="Travis <al.gg@47deg.com>" -m "[skip ci] Generate site"
-git push origin master
+git commit -m "[skip ci] Generate site"
+git push https://47deg:$GITHUB_API_KEY@github.com/47deg/org master
 
 # Push built subtree to official website
 git subtree push --prefix=docs https://47deg:$GITHUB_API_KEY@github.com/47deg/47deg.github.io master 1> /dev/null
