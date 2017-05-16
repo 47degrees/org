57ae4113c6227787332c6d05cfa062e9fc2ecab1
Ana Mª Marquez <anamv84@gmail.com>
2017-05-16 17:15:58 +0200

    Update email

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Configure git
-git config --global user.name "developer47deg"
-git config --global user.email "developer@47deg.com"
+git config --global user.name "anamariamv"
+git config --global user.email "ana@47deg.com"
 
 # Pull latest version of published subtree
 git checkout master
-git subtree pull --prefix=docs --message="[skip ci] Update subtree" https://47deg:$GITHUB_API_DEVELOPER_KEY@github.com/47deg/47deg.github.io.git master
+git subtree pull --prefix=docs --message="[skip ci] Update subtree" https://47deg:$GITHUB_API_KEY@github.com/47deg/47deg.github.io.git master
 
 # Build & Commit built site
 lein run
 git add docs
 git commit -m "[skip ci] Generate site"
-git push https://47deg:$GITHUB_API_DEVELOPER_KEY@github.com/47deg/org master
+git push https://47deg:$GITHUB_API_KEY@github.com/47deg/org master
 
 # Push built subtree to official website
-git subtree push --prefix=docs https://47deg:$GITHUB_API_DEVELOPER_KEY@github.com/47deg/47deg.github.io.git master
+git subtree push --prefix=docs https://47deg:$GITHUB_API_KEY@github.com/47deg/47deg.github.io.git master
